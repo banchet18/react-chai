480ece131bc143fd78ca7b00be7f00fd34367c0d
banchet18 <banjarac6@gmail.com>
2024-02-07 17:02:47 +0530

    c

---
+++ 04customReact/customreactpractice.js
@@ -0,0 +1,10 @@
+const mainContainer = document.getElementById("root");
+
+const reactElement = {
+  type: "a",
+  props: {
+    href: "https://www.google.com",
+    target: "blank",
+  },
+  children: "click here",
+};
