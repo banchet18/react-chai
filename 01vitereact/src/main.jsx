1aa1c3612c555c2f4eb437530e8618f69a4be216
banchet18 <banjarac6@gmail.com>
2024-02-07 10:08:47 +0530

    c

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,28 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { createElement } from "react";
+// const reactElement = {
+//   type: "a",
+//   props: {
+//     href: "https://google.com",
+//     target: "_blank",
+//   },
+//   children: "Click me to visit google",
+// };
 
 const anotherElement = (
   <a href="https://google.com" target="blank">
     Visit google
   </a>
 );
-ReactDOM.createRoot(document.getElementById("root")).render(anotherElement);
+
+const reactElement = React.createElement(
+  "a",
+  {
+    href: "https://google.com",
+    target: "blank",
+  },
+  "Click me to visit google"
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(reactElement);
