9bd35a1ef36259b5d8178fd6ea62a8ca7f9bdcea
banchet18 <banjarac6@gmail.com>
2024-02-07 09:52:15 +0530

    c

--- 04customReact/customreact.js
+++ 04customReact/customreact.js
@@ -1,9 +1,19 @@
 function customRender(reactElement, container) {
+  /*
   const domElement = document.createElement(reactElement.type);
   domElement.innerHTML = reactElement.children;
   domElement.setAttribute("href", reactElement.props.href);
   domElement.setAttribute("href", reactElement.props.target);
 
+  container.appendChild(domElement);
+  */
+
+  const domElement = document.createElement(reactElement.type);
+  domElement.innerHTML = reactElement.children;
+  for (const prop in reactElement.props) {
+    if (prop === "children") container;
+    domElement.setAttribute(prop, reactElement.props[prop]);
+  }
   container.appendChild(domElement);
 }
 
