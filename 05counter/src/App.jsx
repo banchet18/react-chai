a2dc0ff76458e8ac7ed6ff5db1c68577dffb6484
banchet18 <banjarac6@gmail.com>
2024-02-07 11:18:50 +0530

    c

--- 05counter/src/App.jsx
+++ 05counter/src/App.jsx
@@ -7,6 +7,10 @@ function App() {
   return (
     <>
       <h1>Chai aur code</h1>
+      <h2>counter:5</h2>
+      <button>Add value</button>
+      <br />
+      <button>remove value</button>
     </>
   );
 }
