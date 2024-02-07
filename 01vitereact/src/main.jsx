3f75fb0e4211dda1b7ad077af35b773d7c9fd74f
banchet18 <banjarac6@gmail.com>
2024-02-07 10:04:13 +0530

    c

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,5 +1,9 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+const anotherElement = (
+  <a href="https://google.com" target="blank">
+    Visit google
+  </a>
+);
+ReactDOM.createRoot(document.getElementById("root")).render(anotherElement);
