09cfbe559a73d6791e529ba5f7b823a6daafb7c8
banchet18 <banjarac6@gmail.com>
2024-02-07 17:33:02 +0530

    c

--- 05counter/src/App.jsx
+++ 05counter/src/App.jsx
@@ -1,33 +1,35 @@
-import { useState } from "react";
-import "./App.css";
+// import { useState } from "react";
+// import "./App.css";
 
-function App() {
-  const [counter, setCounter] = useState(15);
+// function App() {
+//   const [counter, setCounter] = useState(15);
 
-  //let counter = 5;
+//   //let counter = 5;
 
-  const addValue = () => {
-    //counter = counter + 1;
-    if (counter < 20) {
-      setCounter(counter + 1);
-    }
-  };
+//   const addValue = () => {
+//     //counter = counter + 1;
+//     if (counter < 20) {
+//       setCounter(counter + 1);
+//     }
+//   };
 
-  const removeValue = () => {
-    if (counter > 0) {
-      setCounter(counter - 1);
-    }
-  };
+//   const removeValue = () => {
+//     if (counter > 0) {
+//       setCounter(counter - 1);
+//     }
+//   };
 
-  return (
-    <>
-      <h1>Chai aur code</h1>
-      <h2>counter value: {counter}</h2>
-      <button onClick={addValue}>Add value</button>
-      <br />
-      <button onClick={removeValue}>remove value</button>
-    </>
-  );
-}
+//   return (
+//     <>
+//       <h1>Chai aur code</h1>
+//       <h2>counter value: {counter}</h2>
+//       <button onClick={addValue}>Add value</button>
+//       <br />
+//       <button onClick={removeValue}>remove value</button>
+//     </>
+//   );
+// }
 
-export default App;
+// export default App;
+
+// ------------------------------------------------------------
