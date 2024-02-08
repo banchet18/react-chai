89af39aa8003920790a3ed24b97bbe5f9d7f07fe
banchet18 <banjarac6@gmail.com>
2024-02-08 11:59:37 +0530

    c

--- 08passwordGenerator/src/App.jsx
+++ 08passwordGenerator/src/App.jsx
@@ -34,13 +34,36 @@ function App() {
             copy
           </button>
         </div>
-        <div className="flex items-center gap-x-1">
-          <input
-            type="checkbox"
-            defaultChecked={charAllowed}
-            id="characterInput"
-          />
-          <label htmlFor="characterInput">Characters</label>
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x-1">
+            <input
+              type="range"
+              min={6}
+              max={100}
+              value={length}
+              className="cursor-pointer"
+              onChange={(e) => {
+                setLength(e.target.value);
+              }}
+            />
+            <label>Length: {length}</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={numberAllowed}
+              id="numberInput"
+            />
+            <label htmlFor="numberInput">Numbers</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={charAllowed}
+              id="characterInput"
+            />
+            <label htmlFor="characterInput">Characters</label>
+          </div>
         </div>
       </div>
     </>
