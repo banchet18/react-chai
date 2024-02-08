394ea5c3a2844e5c7e06b70a5368e78cf5e850e2
banchet18 <banjarac6@gmail.com>
2024-02-08 10:50:31 +0530

    c

--- 07bgChanger/src/App.jsx
+++ 07bgChanger/src/App.jsx
@@ -8,7 +8,40 @@ function App() {
       className="w-full h-screen duration-200"
       style={{ backgroundColor: color }}
     >
-      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2"></div>
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button
+            className="outline-none px-4 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "red" }}
+          >
+            Red
+          </button>
+          <button
+            className="outline-none px-4 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: " green" }}
+          >
+            green
+          </button>
+          <button
+            className="outline-none px-4 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "blue" }}
+          >
+            blue
+          </button>
+          <button
+            className="outline-none px-4 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "yellow" }}
+          >
+            yellow
+          </button>
+          <button
+            className="outline-none px-4 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "black" }}
+          >
+            black
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
