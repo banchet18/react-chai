521bf90ec970dc52fc90097e28bc21bdeedfaf49
banchet18 <banjarac6@gmail.com>
2024-02-08 11:36:34 +0530

    c

--- 08passwordGenerator/src/App.jsx
+++ 08passwordGenerator/src/App.jsx
@@ -1,12 +1,18 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import "./App.css";
 
 function App() {
   const [length, setLength] = useState(8);
   const [numberAllowed, setNumberAllowed] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false);
-  const [password, setpassword] = useState();
+  const [password, setpassword] = useState("");
 
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (numberAllowed) str += "0123456789";
+    if (numberAllowed) str += "0123456789";
+  }, [length, numberAllowed, charAllowed, setpassword]);
   return (
     <>
       <h1 className=" text-4xl text-center text-white">password generator</h1>
