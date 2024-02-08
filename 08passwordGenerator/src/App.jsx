ed3f87858122a90abb9bd666033e9d46851d25c6
banchet18 <banjarac6@gmail.com>
2024-02-08 11:47:44 +0530

    c

--- 08passwordGenerator/src/App.jsx
+++ 08passwordGenerator/src/App.jsx
@@ -12,10 +12,26 @@ function App() {
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     if (numberAllowed) str += "0123456789";
     if (charAllowed) str += "!@#$%Z^&*(){}[]~`+-";
+
+    for (let i = 1; i <= str.length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass = str.charAt(char);
+    }
+    setpassword(pass);
   }, [length, numberAllowed, charAllowed, setpassword]);
   return (
     <>
-      <h1 className=" text-4xl text-center text-white">password generator</h1>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-700">
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="password"
+            readOnly
+          />
+        </div>
+      </div>
     </>
   );
 }
