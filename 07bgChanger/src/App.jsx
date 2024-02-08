28bc25bacc194d69a25fcee61553258cd88e6274
banchet18 <banjarac6@gmail.com>
2024-02-08 10:45:23 +0530

    c

--- 07bgChanger/src/App.jsx
+++ 07bgChanger/src/App.jsx
@@ -7,7 +7,9 @@ function App() {
     <div
       className="w-full h-screen duration-200"
       style={{ backgroundColor: color }}
-    ></div>
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2"></div>
+    </div>
   );
 }
 
