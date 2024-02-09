77493c340767dfe0c15b6889070410334416af91
banchet18 <banjarac6@gmail.com>
2024-02-09 16:53:40 +0530

    c

--- 10reactRouter/src/components/Github/Github.jsx
+++ 10reactRouter/src/components/Github/Github.jsx
@@ -1,22 +1,31 @@
-import { useState } from "react";
-import { useEffect } from "react";
+// import { useState } from "react";
+// import { useEffect } from "react";
+import { useLoaderData } from "react-router-dom";
 
 const Github = () => {
-  const [data, setData] = useState("");
+  // const [data, setData] = useState("");
 
-  const githubData = async () => {
-    const response = await fetch("https://api.github.com/users/banchet18");
-    const result = await response.json();
-    setData(result);
-  };
-  useEffect(() => {
-    githubData();
-  }, []);
+  // const githubData = async () => {
+  //   const response = await fetch("https://api.github.com/users/banchet18");
+  //   const result = await response.json();
+  //   setData(result);
+  // };
+  // useEffect(() => {
+  //   githubData();
+  // }, []);
+
+  const data = useLoaderData();
 
   return (
     <div className=" text-center m-4 bg-gray-400 text-white p-4 text-3xl">
       <img src={data.avatar_url} width={300} alt="" />
+      Github Followers : {data.followers}
     </div>
   );
 };
 export default Github;
+
+export const githubInfoLoader = async () => {
+  const response = await fetch(`https://api.github.com/users/banchet18`);
+  return response.json();
+};
--- 10reactRouter/src/main.jsx
+++ 10reactRouter/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+// import App from "./App.jsx";
 import "./index.css";
 import {
   Route,
@@ -14,6 +14,7 @@ import About from "./components/About/About.jsx";
 import Contact from "./components/Contact/Contact.jsx";
 import User from "./components/User/User.jsx";
 import Github from "./components/Github/Github.jsx";
+import githubInfoLoader from "./components/Github/Github.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -24,7 +25,7 @@ const router = createBrowserRouter(
       <Route path="user/" element={<User />}>
         <Route path=":userid" element={<User />} />
       </Route>
-      <Route path="github" element={<Github />} />
+      <Route loader={githubInfoLoader} path="github" element={<Github />} />
       <Route path="*" element={<div>Not Found</div>} />
     </Route>
   )
